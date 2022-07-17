3bbf409148e4cd9df443543b84bcdeb874983da6
Christer <christer@nerv.r.cji.lan>
2022-07-17 19:22:19 +0200

    Error handling, write additional info to output dir

--- rollerblades.sh
+++ rollerblades.sh
@@ -6,8 +6,26 @@ if ! [[ -d "$SCRIPT_DIR" ]]; then
 	exit 1
 fi
 
+
+
 CFG_DIR="${SCRIPT_DIR}/cfg"
 REPOS_DIR="${SCRIPT_DIR}/repos"
+
+if ! [[ -d "$REPOS_DIR" ]]; then
+	echo "Error: Could not locate repos directory"
+	exit 1
+fi
+
+if ! [[ -f "$CFG_DIR/settings.txt" ]]; then
+	echo "Error: Settings file does not exist"
+	exit 1
+fi
+
+
+if ! [[ -f "$CFG_DIR/repos.txt" ]]; then
+	echo "Error: Repos file does not exist"
+	exit 1
+fi
 
 # load cfg
 source "${CFG_DIR}/settings.txt"
@@ -39,11 +57,12 @@ deploy (){
 
 		cd "${repo_dir}"
 		git archive --format=tar HEAD | gzip > "${OUTPUT_DIR}/${repo}.tar.gz"
+		echo "$date" > "${OUTPUT_DIR}/${repo}.updated.txt"
 		cd ..
 		echo "Finished processing '$repo'"
 		echo
 	done < "${CFG_DIR}/repos.txt"
-	
+
 }
 
 while true; do
@@ -51,6 +70,7 @@ while true; do
 	header
 	echo "Downloading and publishing repos"
 	deploy
+	echo "Rollerblades - Latest deployment: $(date), next update in ${SLEEP_TIME}" > "${OUTPUT_DIR}/index.html"
 	echo '# # # # # # # #'
 	echo "$(date): Done, sleeping (${SLEEP_TIME})"
 	sleep "${SLEEP_TIME}"
