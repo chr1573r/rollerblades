4423b190acd9d453c1b9de229eab4f3c195e3eb0
Christer Jonassen <christer@csdnserver.com>
2023-05-18 13:58:37 +0200

    Updated readme for signing and motd options. Also added overall description

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # rollerblades
 Distribution tool
 
+Rollerblades can be used to download and publish git repo content as tar.gz archives.
+These tar archives can be signed by rollerblades, so that clients can verify them against a distributed public key.
+
 # setup
 make a dir called 'cfg' and 'repos'
 in 'cfg', make a file called 'repos.txt'. Add your repo names here.
 
 in 'cfg', make a file called 'settings.txt'. Rollerblades will source this files and expect some vars to be set here.
+You can reference variables used internally by rollerblades.sh, such as SCRIPT_DIR and CFG_DIR in settings.
 
 'settings.txt' sample
 ```# How often repos are downloaded and deployed
@@ -13,6 +17,14 @@ SLEEP_TIME=5m
 
 # Where to store archives
 OUTPUT_DIR=/home/christer/www
+
+# Optional content that will be inserted at the beginning of index.html in the output directory
+MOTD=/home/christer/rollerblades_motd.txt
+
+# Signing configuration
+SIGNING=true
+SIGNING_PRIVATE_KEY="$CFG_DIR/rollerblades.key"
+SIGNING_PUBLIC_KEY="$CFG_DIR/rollerblades.pub"
 
 # Git clone prefix/suffix
 CLONE_PREFIX=git@github.com:chr1573r
--- rollerblades.sh
+++ rollerblades.sh
@@ -30,9 +30,6 @@ fi
 # load cfg
 source "${CFG_DIR}/settings.txt"
 
-gfx_init(){
-	reset
-}
 
 header(){
 	echo '|)  || _   | |   | _  _'
@@ -40,12 +37,21 @@ header(){
 	echo
 }
 
+sign(){ # sign <key> <signature output> <file to sign>
+	openssl dgst -sha256 -sign "$1" -out "$2" "$3"
+}
+sign_verify(){ # sign_check <public key> <signature file> <file to verify>
+	openssl dgst -sha256 -verify "$1" -signature "$2" "$3"
+}
+
 deploy (){
+	repo_count=0
 	while IFS= read -r repo; do
 		url="${CLONE_PREFIX}/${repo}${CLONE_SUFFIX}"
 		repo_dir="${REPOS_DIR}/${repo}"
-
+		release="${OUTPUT_DIR}/${repo}.tar.gz"
 		echo "Processing '$repo'"
+		((repo_count++))
 
 		if [[ -d "${repo_dir}" ]]; then
 			cd "${repo_dir}"
@@ -56,21 +62,39 @@ deploy (){
 		fi
 
 		cd "${repo_dir}"
-		git archive --format=tar HEAD | gzip > "${OUTPUT_DIR}/${repo}.tar.gz"
+		git archive --format=tar HEAD | gzip > "${release}"
+		if "$SIGNING"; then
+			echo "Signing release.."
+			sign "$SIGNING_PRIVATE_KEY" "${release}.signature" "${release}"
+			echo -n "Checking signature.. "
+			sign_verify "$SIGNING_PUBLIC_KEY" "${release}.signature" "${release}" && ((repo_success++))
+		elif [[ -f "${release}" ]]; then
+			((repo_success++))	
+		fi
 		echo "$date" > "${OUTPUT_DIR}/${repo}.updated.txt"
 		cd ..
 		echo "Finished processing '$repo'"
 		echo
 	done < "${CFG_DIR}/repos.txt"
+}
 
+report(){
+if [[ -f "$MOTD" ]]; then
+		cat "$MOTD" > "${OUTPUT_DIR}/index.html"
+	else
+		echo -n > "${OUTPUT_DIR}/index.html"
+	fi
+	echo "Rollerblades - Latest deployment: $(date). ${repo_success}/${repo_count} repos deployed successfully. Next update in ${SLEEP_TIME}" >> "${OUTPUT_DIR}/index.html"
 }
 
 while true; do
-	gfx_init
 	header
+	echo "$(date): Start"
 	echo "Downloading and publishing repos"
 	deploy
-	echo "Rollerblades - Latest deployment: $(date), next update in ${SLEEP_TIME}" > "${OUTPUT_DIR}/index.html"
+	echo "${repo_success}/${repo_count} repos deployed successfully"
+	echo "Updating report"
+	report
 	echo '# # # # # # # #'
 	echo "$(date): Done, sleeping (${SLEEP_TIME})"
 	sleep "${SLEEP_TIME}"
