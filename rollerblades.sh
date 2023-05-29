6e3b6760fd5f711e559ca80914f8edc8a1b0a2c7
Christer Jonassen <christer@csdnserver.com>
2023-05-29 15:19:53 +0200

    Improved logging, changes in web logging

--- rollerblades.sh
+++ rollerblades.sh
@@ -6,8 +6,6 @@ if ! [[ -d "$SCRIPT_DIR" ]]; then
 	exit 1
 fi
 
-
-
 CFG_DIR="${SCRIPT_DIR}/cfg"
 REPOS_DIR="${SCRIPT_DIR}/repos"
 
@@ -30,75 +28,147 @@ fi
 # load cfg
 source "${CFG_DIR}/settings.txt"
 
+# determine if internal logging will be enabled
+if [[ -z "$LOG_FILE" ]]; then
+	rb_feature_filelog="false"
+else
+	rb_feature_filelog="true"
+fi
 
-header(){
-	echo '|)  || _   | |   | _  _'
-	echo '|\()||(/_|`|)|(|(|(/__\'
-	echo
+# print line to console and optionally internal log
+ut(){
+	echo "$*"
+	log "$*"
 }
 
+# print line without line terminator to console and optionally internal log
+utn(){
+	echo -n "$*"
+	logn "$*"
+}
+
+# print header to console and optionally internal log
+header(){
+	ut '|)  || _   | |   | _  _'
+	ut '|\()||(/_|`|)|(|(|(/__\'
+	ut
+}
+
+# create file signature with public key
 sign(){ # sign <key> <signature output> <file to sign>
 	openssl dgst -sha256 -sign "$1" -out "$2" "$3"
 }
+
+# verify a signed file
 sign_verify(){ # sign_check <public key> <signature file> <file to verify>
 	openssl dgst -sha256 -verify "$1" -signature "$2" "$3"
 }
 
+# log to console, internal log (optional), and published log
+multilog(){ 
+	ut "$*"
+	weblog "$*"
+}
+
+# log to internal log 
+log(){ 
+	if "$rb_feature_filelog"; then
+		echo "$*" >> "${LOG_FILE}"
+	fi
+}
+
+# log to internal log without line terminator
+logn(){ 
+	if "$rb_feature_filelog"; then
+		echo -n "$*" >> "${LOG_FILE}"
+	fi
+}
+
+#init published log file. Uses motd file as template if specified
+weblog_init(){ 
+	if [[ -f "$MOTD" ]]; then
+		cp "$MOTD" "${OUTPUT_DIR}/rollerblades.log"
+	else
+		echo -n > "${OUTPUT_DIR}/rollerblades.log"
+	fi
+
+}
+
+#log to published log
+weblog(){
+	echo "$*" >> "${OUTPUT_DIR}/rollerblades.log"
+}
+
+#copy published log to index.html and wrap with pre tags
+weblog_html(){ 
+	echo '<pre>' > "${OUTPUT_DIR}/index.html"
+	cat "${OUTPUT_DIR}/rollerblades.log" >> "${OUTPUT_DIR}/index.html"
+	echo '</pre>' >> "${OUTPUT_DIR}/index.html"
+}
+
+# download and publish repos
 deploy (){
 	repo_count=0
+	# Process each repo
 	while IFS= read -r repo; do
 		url="${CLONE_PREFIX}/${repo}${CLONE_SUFFIX}"
 		repo_dir="${REPOS_DIR}/${repo}"
 		release="${OUTPUT_DIR}/${repo}"
-		echo "Processing '$repo'"
+		ut "##### Processing '$repo' #####"
 		((repo_count++))
 
+		# Pull repo
 		if [[ -d "${repo_dir}" ]]; then
 			cd "${repo_dir}"
 			git pull
+			repo_git_status="$?"
 			cd ..
 		else
 			git -C "${REPOS_DIR}" clone "$url"
+			repo_git_status="$?"
+		fi
+		
+		# Publish repo if pull was OK
+		if [[ "$repo_git_status" -eq 0 ]]; then
+			cd "${repo_dir}"
+			git archive --format=tar HEAD | gzip > "${release}.tar.gz"
+				if "$SIGNING"; then
+					ut "Signing release.."
+					sign "$SIGNING_PRIVATE_KEY" "${release}.signature" "${release}.tar.gz"
+					utn "Checking signature.. "
+					if ut $(sign_verify "$SIGNING_PUBLIC_KEY" "${release}.signature" "${release}.tar.gz"); then
+						((repo_success++))
+						date > "${release}.updated"
+					fi
+				elif [[ -f "${release}.tar.gz" ]]; then
+					((repo_success++))
+					date > "${release}.updated"
+				fi
+			cd ..
+		else
+			ut "Git pull failed for '$repo'"
 		fi
 
-		cd "${repo_dir}"
-		git archive --format=tar HEAD | gzip > "${release}.tar.gz"
-		if "$SIGNING"; then
-			echo "Signing release.."
-			sign "$SIGNING_PRIVATE_KEY" "${release}.signature" "${release}.tar.gz"
-			echo -n "Checking signature.. "
-			if sign_verify "$SIGNING_PUBLIC_KEY" "${release}.signature" "${release}.tar.gz"; then
-				((repo_success++))
-				date > "${release}.updated"
-			fi
-		elif [[ -f "${release}.tar.gz" ]]; then
-			((repo_success++))
-			date > "${release}.updated"
-		fi
-		cd ..
-		echo "Finished processing '$repo'"
-		echo
+		ut "#####  Finished '$repo'  #####"
 	done < "${CFG_DIR}/repos.txt"
 }
 
-report(){
-if [[ -f "$MOTD" ]]; then
-		cat "$MOTD" > "${OUTPUT_DIR}/index.html"
-	else
-		echo -n > "${OUTPUT_DIR}/index.html"
-	fi
-	echo "Rollerblades - Latest deployment: $(date). ${repo_success}/${repo_count} repos deployed successfully. Next update in ${SLEEP_TIME}" >> "${OUTPUT_DIR}/index.html"
-}
 
+# main loop
 while true; do
+	weblog_init
 	header
-	echo "$(date): Start"
-	echo "Downloading and publishing repos"
+	ut "$(date): Start"
+	ut "Downloading and publishing repos"
 	deploy
-	echo "${repo_success}/${repo_count} repos deployed successfully"
-	echo "Updating report"
-	report
-	echo '# # # # # # # #'
-	echo "$(date): Done, sleeping (${SLEEP_TIME})"
-	sleep "${SLEEP_TIME}"
+	multilog "Rollerblades - Latest deployment: $(date)."
+	multilog "${repo_success}/${repo_count} repos deployed successfully."
+	weblog_html
+	ut '# # # # # # # #'
+	if ! [[ -z "${SLEEP_TIME}" ]]; then
+		ut "Sleeping (${SLEEP_TIME})"
+		sleep "${SLEEP_TIME}"
+	else
+		exit
+	fi
 done
